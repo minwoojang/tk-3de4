9ec7baa6ba6adff8b0df75b3f6112a5d4744be3f
minwoojang <q860514@naver.com>
2023-12-27 11:35:29 +0900

    check

--- startup/startup.py
+++ startup/startup.py
@@ -5,18 +5,22 @@ import os
 import sys
 import tde4
 #from PySide2 import QtGui, QtCore, QtWidgets
-
-sys.path.append("O:\\inhouse\\rez-packages\\PySide\\1.2.4\\platform-windows\\arch-AMD64\\python")
-
+# sys.path.append("O:\\inhouse\\Python\\Python37\\Lib\\site-packages")
+# sys.path.append("O:\\inhouse\\rez-packages\\PySide\\1.2.4\\platform-windows\\arch-AMD64\\python")
 if tde4.get3DEVersion().split()[-1] > '6':
+    # sys.path.append("O:\\inhouse\\rez-packages\\PySide2\\5.12.9\\platform-windows\\arch-AMD64\\site-packages")
     from Qt.QtCore import *
     from Qt.QtGui import *
     from Qt.QtWidgets import *
 else:
-    from PySide.QtGui import *
-    from PySide.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtCore import *
+    
 
-import tank
+sys.path.append(
+    os.path.join(os.getenv('TANK_CURRENT_PC'), 'install', 'core', 'python')
+)
+import sgtk
 
 def _timer():
     QCoreApplication.processEvents()
@@ -26,31 +30,21 @@ def _timer():
     cur_file = tde4.getProjectPath()
     if g_current_file != cur_file:
         if cur_file:
-            engine = tank.platform.current_engine()
+            engine = sgtk.platform.current_engine()
             context = engine.context
-            new_context = engine.tank.context_from_path(cur_file, context)
+            new_context = engine.sgtk.context_from_path(cur_file, context)
             if new_context != context:
-                tank.platform.change_context(new_context)
+                sgtk.platform.change_context(new_context)
         g_current_file = cur_file
 
 if __name__ == '__main__':
-    sys.path.append(
-        os.path.join(
-            os.getenv("TANK_CURRENT_PC"),
-            "install",
-            "core",
-            "python",
-        )
-    )
-    import sgtk
-    
-    engine = tank.platform.current_engine()
+    engine = sgtk.platform.current_engine()
     if not engine:
         from tank_vendor.shotgun_authentication import ShotgunAuthenticator
-        user = ShotgunAuthenticator(tank.util.CoreDefaultsManager()).get_user()
-        tank.set_authenticated_user(user)
-        context = tank.context.deserialize(os.environ.get("TANK_CONTEXT"))
-        engine = tank.platform.start_engine('tk-3de4', context.tank, context)
+        user = ShotgunAuthenticator(sgtk.util.CoreDefaultsManager()).get_user()
+        sgtk.set_authenticated_user(user)
+        context = sgtk.context.deserialize(os.environ.get("TANK_CONTEXT"))
+        engine = sgtk.platform.start_engine('tk-3de4', context.sgtk, context)
 
     # Qt
     if not QCoreApplication.instance():
